f8c3030db917cb0ca5cf0bbb926a601da986b5c5
Ahmet Babalı <ahmetbabaliq@gmail.com>
2021-10-07 01:21:42 +0300

    test2

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    /*
-     * IEntity implement eden class bir veritabanı tablosudur.
-     */
+    // IEntity implement eden class bir veritabanı tablosudur.
 
-    // Bu kalıtım alan class için yorum yaparken veritabanına karşılık gelen  bir tablo olduğunu işaretlemiş oluyoruz
+    // Bu kalıtım alan class için yorum yaparken veritabanına karşılık gelen bir tablo olduğunu işaretlemiş oluyoruz
     public interface IEntity
     {
     }
